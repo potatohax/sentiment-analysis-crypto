5a4a7c3a97170a6c5c867db10a972d12129bc5e7
glen <gargl791@student.otago.ac.nz>
2024-01-27 19:31:32 +1300

    working df of sentiment data

--- src/get_data.py
+++ src/get_data.py
@@ -2,6 +2,7 @@ import os
 import requests
 import sentiment_analysis as sa
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 
@@ -9,11 +10,34 @@ API_KEY = os.environ.get("myapi")
 req_url = os.environ.get("request_url")
 
 req = requests.get(req_url)
-title = len(req.json()["results"][0]["title"])
 
-result = sa.analyze_text(title)
+news_list = req.json()["results"]
+title = len(req.json()["results"])
+news_header = []
+data = []
+sentiment_score = []
 
-print(title)
+#Traverse through entire news list and add data into lists
+for i in range(len(news_list)):
+    curr = news_list[i]["title"]
+    print(curr)
+    result = sa.analyze_text(curr)
+    news_header.append(curr)
+    data.append(result)
+    sentiment_score.append(data[i][2] - data[i][0])
+    
+dict = {
+    "header" : news_header,
+    "sentiment_result" : data,
+    "score" : sentiment_score
+}
+df = pd.DataFrame(data = dict)
+
+
+print(df)
+print("length:", len(df))
+print(sum(df["score"])/len(df))
+
 
 
 
